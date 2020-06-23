6912b12775ae2e60dd29c8c5fb36ed262ca5ec93
sangjin1993 <hardoner@naver.com>
2020-06-23 15:53:42 +0900

    movie app wraming-up start & end

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
 import React from 'react';
 
 class App extends React.Component{
-
-  constructor(props){
-    super(props);
-    console.log('hello');
-  }
-
   state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState(current => ({
-      count: current.count + 1,
-    }));
-  };
-
-  minus = () => {
-    this.setState(current => ({
-      count: current.count - 1,
-    }));
+    isLoading: true,
+    movies: [],
   };
 
   componentDidMount() {
-    console.log('component rendered');
+    // 영화 데이터 로딩!
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    }, 6000);
   }
 
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
-
-  componentWillUnmount(){
-    console.log('Goodbye, cruel world');
-  }
-
-  render(){
-    console.log("I'm renering");
-    return (
-      <div>
-        <hi>The number is: {this.state.count}</hi>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
 }
 
