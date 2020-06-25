028c08869d06007aedef18f9ccee6dbd003bdd67
sangjin1993 <hardoner@naver.com>
2020-06-25 17:26:40 +0900

    Updates

---
+++ precache-manifest.cf8055b3695d5f94dbafd9570233693b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb07c8b8c1039f825e68f7cd071dae3",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "138a6f476e923a930829",
+    "url": "/movie_app_2020/static/css/main.6c5fe2f5.chunk.css"
+  },
+  {
+    "revision": "71c215e5f1ab0eaef47d",
+    "url": "/movie_app_2020/static/js/2.27ddaf76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.27ddaf76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "138a6f476e923a930829",
+    "url": "/movie_app_2020/static/js/main.8cb5d60e.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
