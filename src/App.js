4925acd826286259b172b21e27b6bc24be345d6b
sangjin1993 <hardoner@naver.com>
2020-06-25 17:24:21 +0900

    완성 코드

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 function App(){
   return (
@@ -12,6 +13,7 @@ function App(){
       <Route path="/" exact={true} component={Home} >
       </Route>
       <Route path="/about" component={About}/>
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
